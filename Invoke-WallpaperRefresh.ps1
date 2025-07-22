182f8e5e1a05729fdb60ace7a9e64c819e334d92
hamilton-scott <wcsh86@gmail.com>
2025-07-22 14:25:34 -0400

    Create Invoke-WallpaperRefresh.ps1

---
+++ Invoke-WallpaperRefresh.ps1
@@ -0,0 +1,22 @@
+# This will refresh the wallpaper on a Windows 10/11 machine without requiring a restart of explorer.exe
+
+$imgPath="c:\Path\To\Wallpaper.jpg"
+$code = @' 
+using System.Runtime.InteropServices; 
+namespace Win32{ 
+    
+     public class Wallpaper{ 
+        [DllImport("user32.dll", CharSet=CharSet.Auto)] 
+         static extern int SystemParametersInfo (int uAction , int uParam , string lpvParam , int fuWinIni) ; 
+         
+         public static void SetWallpaper(string thePath){ 
+            SystemParametersInfo(20,0,thePath,3); 
+         }
+    }
+ } 
+'@
+
+add-type $code 
+
+#Apply the Change on the system 
+[Win32.Wallpaper]::SetWallpaper($imgPath)
